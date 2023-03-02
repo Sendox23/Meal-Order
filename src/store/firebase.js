ef174a21874fe05fcb9324840f52893d9a17af19
sendox23 <sendox@protonmail.com>
2023-03-01 18:57:00 -0800

    first init

---
+++ src/store/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1jjA-plAuUrbl5qnY-c-LR2-WtPSDuvw",
+  authDomain: "reactmeals-6b983.firebaseapp.com",
+  databaseURL: "https://reactmeals-6b983-default-rtdb.firebaseio.com",
+  projectId: "reactmeals-6b983",
+  storageBucket: "reactmeals-6b983.appspot.com",
+  messagingSenderId: "855790945737",
+  appId: "1:855790945737:web:a3d11c9843e94991429418"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
